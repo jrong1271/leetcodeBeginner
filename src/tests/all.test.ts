09a13397b1dbdcd90bcde300c05baeceab412236
Joe Rong <jmrong@gmail.com>
2025-02-04 14:33:30 -0500

    add mergeAlternately to utils

--- src/tests/all.test.ts
+++ src/tests/all.test.ts
@@ -7,6 +7,15 @@ import { maximumWealth } from "../utils/1672.maximumWealth";
 import { middleNode, createLinkedList } from "../utils/876.middleNode";
 import { fizzBuzz } from "../utils/412.fizzBuzz";
 import { canConstruct } from "../utils/283.canConstruct";
+import { mergeAlternately } from "../utils/1768.mergeAlternately";
+
+describe("mergeAlternately", () => {
+  it("given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.", () => {
+    expect(mergeAlternately("abc", "pqr")).toEqual("apbqcr");
+    expect(mergeAlternately("ab", "pqrs")).toEqual("apbqrs");
+    expect(mergeAlternately("abcd", "pq")).toEqual("apbqcd");
+  });
+});
 
 describe("canConstruct", () => {
   it("Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise", () => {
---
+++ src/utils/1768.mergeAlternately.ts
@@ -0,0 +1,25 @@
+/* 
+  * Source link: https://leetcode.com/problems/merge-strings-alternately/description/ 
+  * Input: word1 = "abc", word2 = "pqr"
+  * Output: "apbqcr"
+*/
+export function mergeAlternately(word1: string, word2: string): string {
+  let steps;
+  let result: string[] = word1.split('');
+  if ( word1.length > word2.length ) {
+    steps = word2.length;
+  } else {
+    steps = word1.length
+  }
+  for (let i = 0; i <= steps; i++) {
+    if (i<steps) {
+      result[i] += word2[i];
+    }
+    if ( i === steps) {
+      if (word2.length > steps) {
+        result.push(word2.slice(steps));
+      }
+    }
+  }
+  return result.join("");
+};
